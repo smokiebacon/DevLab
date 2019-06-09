9eddb4887898100ea5c50d4dbad87d7238ce957e
Abraham Hsu <smokiebacon@gmail.com>
2019-06-09 15:23:40 -0700

    adds MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+//connect db
+connectDB();
+
 app.get('/', (req, res) => res.send('API Working'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
