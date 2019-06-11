689d3723a264c6f165fb7a96b5359783faa205b5
Abraham Hsu <smokiebacon@gmail.com>
2019-06-10 19:16:15 -0700

    Adds register validation with express-validator/check

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 // @route POST api/users
 // @description Register users
 // @access public
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('User route working')
-})
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+        'password',
+        'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+
+],
+
+
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        console.log(req.body);
+        res.send('User route working')
+    })
 
 module.exports = router;
