1282993a6dbc7508bb09fb4dc35c7ea9b2375dbe
Abraham Hsu <smokiebacon@gmail.com>
2019-06-10 23:55:00 -0700

    adds JSON web tokens upon register

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -53,10 +55,23 @@ router.post('/', [
             const salt = await bcrypt.genSalt(10);
             user.password = await bcrypt.hash(password, salt);
             await user.save();
-            res.send('User registered');
             //return jsonwebtoken
 
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
+            jwt.sign(
+                payload,
+                config.get('jwtToken'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token })
+                }
+            );
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server Error');
