5b8e94a2b6d1e068da8a066273ea2a31f72385d1
Abraham Hsu <smokiebacon@gmail.com>
2019-06-11 22:09:59 -0700

    fixes login logic error with NOT user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -52,7 +52,7 @@ router.post('/', [
         try {
             let user = await User.findOne({ email });
             //check if User exists
-            if (user) {
+            if (!user) {
                 return res.status(400).json({ errors: [{ msg: 'Invalid email / password' }] });
             }
 
