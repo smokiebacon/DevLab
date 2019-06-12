daecb534dcdc69bb24effabeca9379f0cf75a5e3
Abraham Hsu <smokiebacon@gmail.com>
2019-06-11 22:23:13 -0700

    moves code to auth api for better structure

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,12 +4,82 @@ const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 const config = require('config');
 const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
 const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator/check');
 
+
+
+// @route POST api/auth/register
+// @description Register users
+// @access public
+router.post('/register', [
+    // using the package express-validator, check for fields if correct. refer to documentation
+    //check takes in 2 arguments, input and message.
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+        'password',
+        'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+            //check if User exists
+            if (user) {
+                return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+            }
+            //grabs users Gravatar
+            const avatar = gravatar.url(email, {
+                s: '200', //size of 200
+                r: 'pg', //sets gravatar to PG. no naked!
+                d: 'mm' //sets default image
+            })
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            })
+            //encrypt Password with Bcrypt
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+            //return jsonwebtoken
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(
+                payload,
+                config.get('jwtToken'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token })
+                }
+            );
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    })
+
 // @route GET api/auth
 // @access public
-
 //including auth as second argument makes the route protected.
 router.get('/', auth, async (req, res) => {
     try {
@@ -27,11 +97,11 @@ router.get('/', auth, async (req, res) => {
 )
 
 
-// @route POST api/auth
+// @route POST api/auth/login
 //login
 // @description Authenicate user and get token
 // @access public
-router.post('/', [
+router.post('/login', [
     // using the package express-validator, check for fields if correct. refer to documentation
     //check takes in 2 arguments, input and message.
     check('email', 'Please include a valid email').isEmail(),
@@ -53,13 +123,13 @@ router.post('/', [
             let user = await User.findOne({ email });
             //check if User exists
             if (!user) {
-                return res.status(400).json({ errors: [{ msg: 'Invalid email / password' }] });
+                return res.status(400).json({ errors: [{ msg: 'Invalid email or password' }] });
             }
 
             //match password
             const isMatch = await bcrypt.compare(password, user.password);
             if (!isMatch) {
-                return res.status(400).json({ errors: [{ msg: 'Invalid email / password' }] });
+                return res.status(400).json({ errors: [{ msg: 'Invalid email or password' }] });
 
             }
             //return jsonwebtoken
