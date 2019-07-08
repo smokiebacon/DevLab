cf3dfb0fbd226a2375cbd7fb44f38f835bf32eb5
Abraham Hsu <smokiebacon@gmail.com>
2019-07-07 19:45:01 -0700

    adds Vuex store

---
+++ client/src/store/actions.type.js
@@ -0,0 +1,3 @@
+export const ARTICLE_PUBLISH = "publishArticle";
+export const LOGIN = "login";
+export const REGISTER = "register";
---
+++ client/src/store/auth.module.js
@@ -0,0 +1,23 @@
+
+const state = {
+    user: {}
+}
+
+const getters = {
+
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+export default {
+    state,
+    actions,
+    mutations,
+    getters
+};
